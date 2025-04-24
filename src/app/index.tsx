910ae8fc610a803122f3b5efed374628fd6bf7f0
Quang Trong <80852103+quangtrong1506@users.noreply.github.com>
2025-04-24 05:44:06 +0000

    fix Chưa cấp quyền thông báo

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,37 +1,45 @@
 import React, { useEffect, useState } from 'react';
 import { Text, TextInput, View } from 'react-native';
-import messaging from '@react-native-firebase/messaging'
+import messaging from '@react-native-firebase/messaging';
 import { getFcmToken, sendWindowNotification } from '@/helpers';
-import { Link } from 'expo-router';
 
 export default function App() {
-  const [token, setToken] = useState<string | null>(null);
+    const [token, setToken] = useState<string | null>(null);
 
-  useEffect(() => {
-     getFcmToken().then(tk=>{
-       setToken(tk)
+    useEffect(() => {
+        const firstHandle = async () => {
+            const authStatus = await messaging().requestPermission();
+            const enabled =
+                authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+                authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+            if (!enabled) {
+                sendWindowNotification('Chưa cấp quyền thông báo, Không thể nhận thông báo nền');
+                throw new Error('Chưa cấp quyền thông báo');
+            }
+            getFcmToken()
+                .then((tk) => {
+                    setToken(tk);
+                })
+                .catch((e) => {
+                    sendWindowNotification(e);
+                });
+        };
 
-     }).catch(e=>{
-      sendWindowNotification(e)
-     })
-    // Không xử lý khi app đang foreground để tránh trùng socket
-    const unsubscribe = messaging().onMessage(async remoteMessage => {
-      sendWindowNotification('⚠️ Đang foreground, bỏ qua notification: '+ remoteMessage)
-    })
+        firstHandle();
 
-    return unsubscribe
-  }, [])
+        // Không xử lý khi app đang foreground để tránh trùng socket
+        const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+            sendWindowNotification('⚠️ Đang foreground, bỏ qua notification: ' + remoteMessage);
+        });
 
+        return unsubscribe;
+    }, []);
 
-
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>🎯 Expo Push Notifications Demo</Text>
-      {token && <Text>Your Expo Push Token:</Text>}
-      {token&&<TextInput value={token}/>}
-      <Link className="mt-6 inline-block p-3 bg-blue-200" href={'/admin'}>
-        Admin
-      </Link>
-    </View>
-  );
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>🎯 Expo Push Notifications Demo</Text>
+            {token && <Text>Your Expo Push Token:</Text>}
+            {token && <TextInput value={token} />}
+        </View>
+    );
 }
