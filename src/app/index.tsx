b823b4310d1a9bf7b70255a0658c24c43b71014b
Quang Trong <80852103+quangtrong1506@users.noreply.github.com>
2025-04-24 15:34:39 +0700

    thêm implementation 'com.google.firebase:firebase-messaging:23.1.0'

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,7 +5,6 @@ import { getFcmToken, requestAndroidNotificationPermission, sendWindowNotificati
 
 export default function App() {
     const [token, setToken] = useState<string | null>(null);
-    const [loaded, setLoaded] = useState<Boolean>(false);
 
     useEffect(() => {
         const firstHandle = async () => {
@@ -20,7 +19,6 @@ export default function App() {
                 sendWindowNotification('Chưa cấp quyền thông báo, Không thể nhận thông báo nền');
             }
             sendWindowNotification('Đã cấp quyền thông báo');
-            setLoaded(true);
         };
 
         firstHandle();
@@ -28,32 +26,29 @@ export default function App() {
     useEffect(() => {
         sendWindowNotification('Get Token');
 
-        getFcmToken()
-            .then((tk) => {
-                setToken(tk);
-                sendWindowNotification('Token: ' + tk);
-            })
-            .catch((e) => {
-                sendWindowNotification(e);
-            });
-
-        // Không xử lý khi app đang foreground để tránh trùng socket
         const unsubscribe = messaging().onMessage(async (remoteMessage) => {
             sendWindowNotification('⚠️ Đang foreground, bỏ qua notification: ' + remoteMessage);
         });
 
         return unsubscribe;
-    }, [loaded]);
+    }, []);
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text>Push Notifications Demo</Text>
             <View
                 className="bg-blue-500 p-2"
                 onTouchStart={() => {
-                    sendWindowNotification('Click');
+                    getFcmToken()
+                        .then((tk) => {
+                            setToken(tk);
+                            sendWindowNotification('Token: ' + tk);
+                        })
+                        .catch((e) => {
+                            sendWindowNotification(e);
+                        });
                 }}
             >
-                <Text>Ấn vào đây</Text>
+                <Text>Getken</Text>
             </View>
             {token && <Text>Your FCM Token:</Text>}
             {token && <TextInput value={token} />}
