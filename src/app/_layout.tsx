9054c6bf39dd9c1669198e0b402bf0ce320f244e
Quang Trong <80852103+quangtrong1506@users.noreply.github.com>
2025-04-24 15:54:38 +0700

    layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,11 @@
+import { firebase } from '@react-native-firebase/messaging';
 import '../global.css';
 import { Slot } from 'expo-router';
 
+firebase.initializeApp({
+    appId: 'com.quangtrong1506.test.tb',
+    projectId: 'test-tb-nen',
+});
 export default function Layout() {
     return <Slot />;
 }
